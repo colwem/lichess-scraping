ba7813fa41694cd4e607e1c104708cdca41f940b
Martin Colwell <colwem@gmail.com>
2019-06-05 13:44:41 -0400

    Make more resilent

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+def apply_with_sleep(f, lst, slp):
+    if len(lst) == 0:
+        yield None
+    elif len(lst) == 1:
+        yield f(lst[0])
+    else:
+        for l in lst[:-1]:
+            yield f(l)
+            time.sleep(slp)
+        yield f(lst[-1])
 
 def parse_distribution(source):
     soup = BeautifulSoup(source, 'html.parser')
@@ -24,9 +34,12 @@ def parse_distribution(source):
         requests.exceptions.RequestException,
         max_time=300)
 def get(url):
-    time.sleep(2)
     return requests.get(url)
 
+
+@backoff.on_exception(backoff.expo,
+        Exception,
+        max_time=300)
 def get_db():
     try:
         app = firebase_admin.get_app()
@@ -36,6 +49,13 @@ def get_db():
             'projectId': 'lichess-scraping',
         })
     return firestore.client()
+
+@backoff.on_exception(backoff.expo,
+        Exception,
+        max_time=300)
+def write_to_db_with_backoff(db, collection, id, data):
+    dists_ref = db.collection(collection)
+    dists_ref.document(id).set(data)
 
 url_tmpl = 'https://lichess.org/stat/rating/distribution/{perf_type}'
 perf_types = [
@@ -53,26 +73,30 @@ perf_types = [
         'horde',
         'racingKings']
 
+def simple_generator(lst):
+    for l in lst:
+        yield l
+    return None
+
 def scrape_rating_distributions(data, context):
     # Use the application default credentials
     db = get_db()
-    dists_ref = db.collection(u'distributions')
-    for perf_type in perf_types:
-        response = get(url_tmpl.format(perf_type=perf_type))
+    urls = [url_tmpl.format(perf_type=perf_type) for perf_type in perf_types]
+    for response, perf_type in zip(apply_with_sleep(get, urls, 1), perf_types):
         try:
             distribution = parse_distribution(response.text)
-            date = datetime.datetime.now()
-            id = perf_type + date.strftime('%d%m%y%H')
-            dists_ref.document(id).set({
-                u'distribution': distribution,
-                u'date': date,
-                u'perf_type': perf_type
-            })
         except Exception as e:
             print(perf_type)
-            print(response.status)
+            print(response.status_code)
             print(response.text)
             raise e
+
+        date = datetime.datetime.now()
+        id = perf_type + date.strftime('%d%m%y%H')
+        write_to_db_with_backoff(db, u'distributions', id, {
+            u'distribution': distribution,
+            u'date': date,
+            u'perf_type': perf_type})
 
 if __name__ == "__main__":
     scrape_rating_distributions('blah', 'blam')
