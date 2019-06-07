96a31dfabbc6d9ca1b94e2d02a53253389e6f434
Martin Colwell <colwem@gmail.com>
2019-06-06 21:29:42 -0400

    improve chart

--- calculate-distribution-stats/main.py
+++ calculate-distribution-stats/main.py
@@ -6,16 +6,28 @@ def add_percentiles(data, context):
     path_parts = context.resource.split('/documents/')[1].split('/')
     collection_path = path_parts[0]
     document_path = '/'.join(path_parts[1:])
-
     doc = db.collection(collection_path).document(document_path)
-    distribution = data["value"]["fields"]["distribution"]['arrayValues']
 
-    distribution = [d['integerValue'] for d in distribution]
-    ratings = range(812.5, 2813.5, 25)
-    percentiles = [0.05, 0.25, 0.5, 0.75, 0.95]
-    percentile_ratings = get_percentile_ratings(percentiles, distribution)
-    d = {p: r for p, r in zip(percentiles, percentile_ratings)}
-    doc.set(d)
+    distribution = (
+            data["value"]["fields"]["distribution"]['arrayValue']['values'])
+    distribution = [int(d['integerValue']) for d in distribution]
+
+    percentiles = [0.05, 0.25, 0.5, 0.75, 0.95, 0.99]
+
+    bucket_width = 25
+    rating_range_bottom_edges = range(800, 2801, bucket_width)
+
+    main(doc, distribution, rating_range_bottom_edges, percentiles)
+
+def main(doc_ref, distribution, rating_range_bottom_edges, percentiles):
+    percentile_function = get_percentile_function(
+            rating_range_bottom_edges, distribution)
+
+    percentile_ratings = [percentile_function(p) for p in percentiles]
+
+    field_names = [str(int(p * 100)) for p in percentiles]
+    d = {str(p): r for p, r in zip(field_names, percentile_ratings)}
+    doc_ref.update({'percentiles': d})
 
 def first_index_above_n(n, arr):
     for i, c in enumerate(arr):
@@ -23,49 +35,38 @@ def first_index_above_n(n, arr):
             return i
     return None
 
-def get_percentile_ratings(percentiles, dist):
-    # create the cumulative distribution
-    cumulative = []
+def cumulative_distribution(dist):
+    cum = []
     count = 0
     for n in dist:
         count += n
-        cumulative.append(count)
-    total = cumulative[-1]
+        cum.append(count)
+    return cum
 
-    # create the histogram buckets that were used
-    # each value is the bottom edge of the bucket
-    bucket_widths = 25
-    rating_range_bottom_edges = range(800, 2801, bucket_widths)
-
-    # calculate the rating threshold for every percentile requested
-    percentile_ns = [total * p for p in percentiles]
-    percentile_ratings = []
-    for n in percentile_ns:
-        i = first_index_above_n(n, cumulative)
-        miss_distance = n - cumulative[i-1]
-        bucket_count = dist[i]
-        percentage_off = miss_distance / bucket_count
+def get_percentile_function(x, y):
+    cum_y = cumulative_distribution(y)
+    total = cum_y[-1]
+    def fun(p):
+        n = total * p
+        i = first_index_above_n(n, cum_y)
+        percentage_off = (n - cum_y[i-1]) / y[i]
+        bucket_width = x[i+1] - x[i]
         # this isn't the best method
         # should calculate based on percentage of
         # mass under interpolated line
-        percentile_rating = (
-                rating_range_bottom_edges[i]
-                + percentage_off * bucket_widths)
+        return x[i] + percentage_off * bucket_width
+    return fun
 
-        percentile_ratings.append(percentile_rating)
-
-    return percentile_ratings
 
 if __name__ == "__main__":
-    arr = [0] * 81
-    arr[0] =1
-    arr[1] =1
-    arr[2] =1
-    arr[3] =1
-    arr = [3,0,3,3,0,3,2,5,11,17,14,23,28,37,52,67,70,73,78,92,96,120,102,125,
-            86,135,106,92,103,86,100,86,81,100,82,71,90,65,61,66,56,68,44,35,
-            46,32,43,39,48,26,21,14,33,18,14,11,11,6,4,6,1,2,0,0,1,0,1,0,1,0,
-            0,0,0,0,0,0,0,0,0,0,0]
-    print(get_percentile_ratings([0.5], arr))
+    db = get_db()
+    distributions = db.collection('distributions')
+    for doc_ref in distributions.list_documents():
+        distribution = doc_ref.get().get('distribution')
 
+        percentiles = [0.05, 0.25, 0.5, 0.75, 0.95, 0.99]
 
+        bucket_width = 25
+        rating_range_bottom_edges = range(800, 2801, bucket_width)
+
+        main(doc_ref, distribution, rating_range_bottom_edges, percentiles)
--- rating-distribution-reporter/main.py
+++ rating-distribution-reporter/main.py
@@ -1,20 +1,36 @@
 from flask import Flask, jsonify, render_template
 from firestore import get_db
 from itertools import groupby
+import re
 import json
 app = Flask(__name__)
 
 db = get_db()
+
+perf_types = [
+        'bullet',
+        'blitz',
+        'rapid',
+        'classical',
+        'ultraBullet',
+        'crazyhouse',
+        'chess960',
+        'atomic',
+        'kingOfTheHill',
+        'horde',
+        'racingKings',
+        'threeCheck',
+        'antichess', ]
 
 @app.route('/')
 def home():
 
     distributions = db.collection(u'distributions')
     data = [d.to_dict() for d in distributions.stream()]
+    [d.pop('distribution') for d in data]
     for d in data:
         d['date'] = d['date'].strftime('%d%m%y%H')
-
-    return render_template('home.html', data=data)
+    return render_template('home.html', data=data, perf_types=perf_types)
 
 
 @app.route("/download")
@@ -22,3 +38,8 @@ def download_all():
     distributions = db.collection(u'distributions')
     lst = [d.to_dict() for d in distributions.stream()]
     return jsonify(lst)
+
+@app.template_filter()
+def snake_to_title(s):
+    s = s[0].upper() + s[1:]
+    return ' '.join(re.findall('[A-Z][^A-Z]*', s))
